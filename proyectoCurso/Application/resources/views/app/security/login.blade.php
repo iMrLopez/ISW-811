742ed0d8e31cf245bc78193481e29ec54ac73aa2
Marny Lopez (RPA Lead) <marni-andrei.lopez-lopez@dxc.com>
2018-06-12 13:32:33 -0600

    php normalization

---
+++ proyectoCurso/Application/resources/views/app/security/login.blade.php
@@ -0,0 +1,32 @@
+
+        <div class="full-page  section-image" data-color="blue" data-image="../../assets/img/full-screen-image-2.jpg" ;>
+            <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+            <div class="content">
+                <div class="container">
+                    <div class="col-md-4 col-sm-6 ml-auto mr-auto">
+                        <form class="form" method="" action="">
+                            <div class="card card-login card-hidden">
+                                <div class="card-header ">
+                                    <h3 class="header text-center">Inicio de Sesion</h3>
+                                </div>
+                                <div class="card-body ">
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label>Nombre de Usuario (normalmente es tu cedula)</label>
+                                            <input type="text" placeholder="Enter email" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Contraseña</label>
+                                            <input type="password" placeholder="Password" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer ml-auto mr-auto">
+                                    <button type="submit" class="btn btn-warning btn-wd">Iniciar Sesion</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ proyectoCurso/Application/resources/views/app/security/register.blade.php
@@ -0,0 +1,45 @@
+
+        <div class="full-page  section-image" data-color="green" data-image="../../assets/img/full-screen-image-2.jpg" ;>
+            <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+            <div class="content">
+                <div class="container">
+                    <div class="col-md-4 col-sm-6 ml-auto mr-auto">
+                        <form class="form" method="" action="">
+                            <div class="card card-login card-hidden">
+                                <div class="card-header ">
+                                    <h3 class="header text-center">Inicio de Sesion</h3>
+                                </div>
+                                <div class="card-body ">
+                                    <div class="card-body">
+                                      <div class="form-group">
+                                          <label>Correo electronico</label>
+                                          <input type="email" placeholder="Enter email" class="form-control" required>
+                                      </div>
+                                        <div class="form-group">
+                                            <label>Numero de cedula</label>
+                                            <input type="text" placeholder="Enter username" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Contraseña</label>
+                                            <input type="password" placeholder="Password" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <label class="form-check-label">
+                                                    <input class="form-check-input" type="checkbox" value="" required>
+                                                    <span class="form-check-sign"></span>
+                                                    Terminos de servicio
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer ml-auto mr-auto">
+                                    <button type="submit" class="btn btn-warning btn-wd">Registrarme</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
